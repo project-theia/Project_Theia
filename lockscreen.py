c4d1a09568f0405bdb766c54e15ff2de83a24d0c
Zakaria-Kofiro <kofiro@wisc.edu>
2019-07-10 19:59:07 -0500

    fresh start

--- lockscreen.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-subprocess.call('ls', shell=True)
-
-subprocess.call('/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend', shell=True)
-
-
